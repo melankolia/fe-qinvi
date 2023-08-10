96ae03f2e3e58750175b8f161e3c482fa3878cae
Ageng Setyo Nugroho <ageng.nugroho@nostratech.com>
2023-08-10 10:26:49 +0700

    Change Sound

--- src/components/MenusFloating.vue
+++ src/components/MenusFloating.vue
@@ -4,7 +4,7 @@ import { ref, onMounted } from "vue";
 
 const isPlay: Ref<boolean> = ref(false);
 const usedAudio: Ref<string> = ref(
-  "https://dashboard.qinvi.my.id/storage/Yovie%20&%20Nuno%20-%20Janji%20Suci.mp3"
+  "https://dashboard.qinvi.my.id/storage/Maliq%20&%20D%20Essential%20-%20Himalaya%20Cover%20By%20Dreane.mp3"
 );
 const emit = defineEmits<{
   (e: "fnClick", value: string): void;
