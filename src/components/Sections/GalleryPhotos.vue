1ef725bc52292b71ec8a9bf02cdebe723bd9cc8c
Ageng Setyo Nugroho <ageng.nugroho@nostratech.com>
2023-09-15 13:54:38 +0700

    [Template White] Adjust Gallery

--- src/components/Sections/GalleryPhotos.vue
+++ src/components/Sections/GalleryPhotos.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import ChevronLogo from "@/assets/right-chevron.svg";
 import { ref, onMounted } from "vue";
 import type { Ref } from "vue";
 
@@ -6,12 +7,11 @@ type GalleryPhotoPropsTypes = {
   gallery: string[];
 };
 
-const firstPhotos: Ref<string> = ref("/src/assets/images/Hug.png");
 const imgsRef: Ref<string[]> = ref([]);
-const thumbnailRef: Ref<string[]> = ref([]);
+const thumbnailVerticalRef: Ref<string[]> = ref([]);
+const thumbnailLandscapeRef: Ref<string[]> = ref([]);
 const visibleRef: Ref<boolean> = ref(false);
 const indexRef: Ref<number> = ref(0);
-const loading: Ref<boolean> = ref(false);
 const props = defineProps<GalleryPhotoPropsTypes>();
 
 const onShow = (index: number = 0): void => {
@@ -23,33 +23,72 @@ const onHide = (): void => {
   visibleRef.value = false;
 };
 
-const handleAnimation = (i: number): string => {
-  if (i % 2 == 0) return "fade-down";
-  return "fade-right";
+// Vertical Slider
+const image: Ref<HTMLElement | null> = ref(null);
+const containerImage: Ref<HTMLElement | null> = ref(null);
+
+const sliding: Ref<number> = ref(0);
+const handleNext = (): void => {
+  const containerWidth: number =
+    (containerImage.value && containerImage.value.offsetWidth) || 0;
+
+  if (
+    sliding.value * -1 ==
+    containerWidth * (thumbnailVerticalRef.value.length - 1)
+  )
+    return;
+
+  sliding.value -= containerWidth;
 };
 
-const onLoad = (): void => {
-  const initial: number = thumbnailRef.value.length;
-  loading.value = true;
+const handlePrev = (): void => {
+  if (sliding.value >= 0) return;
 
-  setTimeout(() => {
-    loading.value = false;
-    thumbnailRef.value = [
-      ...thumbnailRef.value,
-      ...props.gallery.filter(
-        (e: string, i: number) => i != 0 && i > initial && i <= initial + 4
-      ),
-    ];
-  }, 1500);
+  const containerWidth: number =
+    (containerImage.value && containerImage.value.offsetWidth) || 0;
+  sliding.value += containerWidth;
+};
+
+// Landscape Slider
+const containerImage2: Ref<HTMLElement | null> = ref(null);
+const slidingLandscape: Ref<number> = ref(0);
+const handleNextLandscape = (): void => {
+  const containerWidth: number =
+    (containerImage2.value && containerImage2.value.offsetWidth) || 0;
+
+  if (
+    slidingLandscape.value * -1 ==
+    containerWidth * (thumbnailLandscapeRef.value.length - 1)
+  )
+    return;
+
+  slidingLandscape.value -= containerWidth;
+};
+
+const handlePrevLandscape = (): void => {
+  if (slidingLandscape.value >= 0) return;
+
+  const containerWidth: number =
+    (containerImage2.value && containerImage2.value.offsetWidth) || 0;
+  slidingLandscape.value += containerWidth;
 };
 
 onMounted(() => {
+  // setTimeout(() => {
+  //   image1.value && image1.value.classList.add("translateX");
+  //   image2.value && image2.value.classList.add("translateX");
+  // }, 3000);
   if (props.gallery.length > 0) {
     imgsRef.value = [...props.gallery];
-    thumbnailRef.value = [
-      ...props.gallery.filter((e: string, i: number) => i != 0 && i <= 4),
-    ];
-    firstPhotos.value = imgsRef.value[0];
+    let midIndex = Math.ceil(imgsRef.value.length / 2);
+
+    imgsRef.value.forEach((e: string, i: number) => {
+      if (i <= midIndex) {
+        thumbnailVerticalRef.value = [...thumbnailVerticalRef.value, e];
+      } else {
+        thumbnailLandscapeRef.value = [...thumbnailLandscapeRef.value, e];
+      }
+    });
   }
 });
 </script>
@@ -82,73 +121,82 @@ onMounted(() => {
         A successful marriage requires falling in love many times, always with
         the same person
       </p>
-      <div class="grid grid-cols-1 gap-2 mb-1 mx-1">
-        <img
-          data-aos="fade-down p-1"
-          data-aos-duration="2500"
-          :src="imgsRef[0]"
-          @click="onShow(0 + 1)"
-        />
-      </div>
-      <div class="grid grid-cols-2">
-        <img
-          :data-aos="handleAnimation(i)"
-          data-aos-duration="2500"
-          v-for="(e, i) in thumbnailRef"
-          :key="i"
-          @click="onShow(i + 1)"
-          :src="e"
-          class="p-1"
-        />
-      </div>
-      <div v-if="loading" class="flex flex-col items-center">
-        <svg
-          class="animate-spin mb-4 mt-6 h-8 w-8 text-white"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
+      <div
+        class="flex flex-col items-center bg-gallery-ov p-4 overflow-hidden my-8"
+      >
+        <!-- Vertical Slider -->
+        <div
+          ref="containerImage"
+          class="flex flex-row overflow-hidden relative"
         >
-          <circle
-            class="opacity-25"
-            cx="12"
-            cy="12"
-            r="10"
-            stroke="currentColor"
-            stroke-width="4"
-          ></circle>
-          <path
-            class="opacity-75"
-            fill="currentColor"
-            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-          ></path>
-        </svg>
-      </div>
-      <button
-        class="button-date w-full mt-6 border border-white bg-yellow-20 py-1.5 px-3 rounded-2xl space-x-2.5 z-10 drop-shadow-md"
-        @click="onLoad()"
-      >
-        <p class="body-2 text-black">Lihat Potret Kami</p>
-      </button>
+          <div
+            class="flex flex-col justify-center absolute left-0 top-0 bottom-0 my-auto"
+          >
+            <button
+              @click="handlePrev"
+              class="button-date py-1.5 px-3 rounded-2xl flex flex-row items-center space-x-2.5 z-10"
+            >
+              <img :src="ChevronLogo" width="24" class="rotate-180" />
+            </button>
+          </div>
+          <div
+            class="flex flex-col justify-center absolute right-0 top-0 bottom-0 my-auto"
+          >
+            <button
+              @click="handleNext"
+              class="button-date py-1.5 px-3 rounded-2xl flex flex-row items-center space-x-2.5 z-10"
+            >
+              <img :src="ChevronLogo" width="24" />
+            </button>
+          </div>
 
-      <!-- <p
-        data-aos="fade-right"
-        data-aos-duration="2500"
-        class="headline-14 text-white mt-10 mb-8"
-      >
-        PREWEDDING
-      </p>
-      <iframe
-        data-aos="fade-up"
-        data-aos-duration="2500"
-        width="100%"
-        height="217"
-        src="https://www.youtube.com/embed/ncF2W8ooUg0"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-        class="rounded-lg mb-4"
-      /> -->
+          <img
+            v-for="(e, i) in thumbnailVerticalRef"
+            ref="image"
+            class="image-slide rounded-xl"
+            :style="`transform: translateX(${sliding}px);`"
+            :src="e"
+            :key="i"
+            @click="onShow(i)"
+          />
+        </div>
+        <!-- Landscape Slider -->
+        <div
+          ref="containerImage2"
+          class="flex flex-row overflow-hidden relative mt-8"
+        >
+          <div
+            class="flex flex-col justify-center absolute left-0 top-0 bottom-0 my-auto"
+          >
+            <button
+              @click="handlePrevLandscape"
+              class="button-date py-1.5 px-3 rounded-2xl flex flex-row items-center space-x-2.5 z-10"
+            >
+              <img :src="ChevronLogo" width="24" class="rotate-180" />
+            </button>
+          </div>
+          <div
+            class="flex flex-col justify-center absolute right-0 top-0 bottom-0 my-auto"
+          >
+            <button
+              @click="handleNextLandscape"
+              class="button-date py-1.5 px-3 rounded-2xl flex flex-row items-center space-x-2.5 z-10"
+            >
+              <img :src="ChevronLogo" width="24" />
+            </button>
+          </div>
+
+          <img
+            v-for="(e, i) in thumbnailLandscapeRef"
+            ref="image"
+            class="image-slide rounded-xl"
+            :style="`transform: translateX(${slidingLandscape}px);`"
+            :src="e"
+            :key="i"
+            @click="onShow(i)"
+          />
+        </div>
+      </div>
     </div>
     <vue-easy-lightbox
       :visible="visibleRef"
